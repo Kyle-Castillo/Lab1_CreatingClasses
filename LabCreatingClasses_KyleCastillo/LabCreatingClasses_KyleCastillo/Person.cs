ab82f58cff0c67fed22e06ce12708a9e101bc1c1
Kyle Castillo <kyle.castillo@edu.sait.ca>
2023-01-25 19:05:52 -0700

    Add files via upload

---
+++ LabCreatingClasses_KyleCastillo/LabCreatingClasses_KyleCastillo/Person.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace LabCreatingClasses_KyleCastillo
+{
+    internal class Person
+    {
+        private int personId;
+        private string firstName;
+        private string lastName;
+        private string favoriteColour;
+        private int age;
+        private bool isWorking;
+
+        public Person(int personId, string firstName, string lastName, string favoriteColour, int age, bool isWorking)
+        {
+            this.personId = personId;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.favoriteColour = favoriteColour;
+            this.age = age;
+            this.isWorking = isWorking;
+
+            
+        }
+
+        public string DisplayPersoninfo
+        {
+            get
+            {
+                Console.WriteLine(personId + ": " + firstName + " " + lastName + "'s favorite colour is " + favoriteColour);
+
+                return firstName;
+            }
+        }
+
+        public string ChangefavoriteColour
+        {
+            get
+            {
+                this.favoriteColour = "white";
+
+                Console.WriteLine(personId + ": " + firstName + " " + lastName + "'s favorite colour is " + favoriteColour);
+
+                return favoriteColour;
+            }
+        }
+
+
+        public string GetAgeInTenYears
+        {
+            get
+            {
+                int ageTenYears;
+
+                ageTenYears = age + 10;
+
+                Console.WriteLine(firstName + lastName + "'s Age in 10 years is: " + ageTenYears);
+
+                return firstName;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return string.Format("PersonId : {0} \nFirstName : {1} \nLastName : {2} \nFavoriteColour : {3} \nAge : {4} \nIsWorking : {5}", personId, firstName, lastName, favoriteColour, age, isWorking);
+        }
+
+
+    }
+}
---
+++ LabCreatingClasses_KyleCastillo/LabCreatingClasses_KyleCastillo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabCreatingClasses_KyleCastillo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Person Ian = new Person(1, "Ian", "Brooks", "Red", 30, true);
+            Person Gina = new Person(2, "Gina", "James", "Green", 18, false);
+            Person Mike = new Person(3, "Mike", "Briscoe", "Blue", 45, true);
+            Person Mary = new Person(4, "Mary", "Beals", "Yellow", 28, true);
+
+            string GinaInfo = Gina.DisplayPersoninfo;
+            Console.WriteLine(Mike.ToString());
+            string IanNewColor = Ian.ChangefavoriteColour;
+            string MaryTenYears = Mary.GetAgeInTenYears;
+
+
+        }
+    }
+}
